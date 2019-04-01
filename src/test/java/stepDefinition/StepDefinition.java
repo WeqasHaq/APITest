ba518263c401c8f1740db5710dd538e7af44ed62
WeqasHaq <weqas.haq@tarkik.com>
2019-04-01 14:20:54 -0400

    added step definition

---
+++ src/test/java/stepDefinition/StepDefinition.java
@@ -0,0 +1,61 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class StepDefinition {
+	
+	@Given("I navigate to Manifest Trade portal")
+	public void i_navigate_to_Manifest_Trade_portal() {
+	
+	}
+
+	@Given("I see Lilo Login account and click on it")
+	public void i_see_Lilo_Login_account_and_click_on_it() {
+	 
+	}
+
+	@Given("I land on Dashboard page i should see Cards display for Crew,Shippers,Consignee,Equipment")
+	public void i_land_on_Dashboard_page_i_should_see_Cards_display_for_Crew_Shippers_Consignee_Equipment() {
+
+	}
+
+	@Given("Manifest Charts should display with Preliminary,Submit,Canceled,Closed,Partialy Closed")
+	public void manifest_Charts_should_display_with_Preliminary_Submit_Canceled_Closed_Partialy_Closed() {
+	
+	}
+
+	@Then("Validate Cards are displaying data")
+	public void validate_Cards_are_displaying_data() {
+
+	}
+
+	@Then("Validate Manifest Chart is displaying information for these status")
+	public void validate_Manifest_Chart_is_displaying_information_for_these_status() {
+	 
+	}
+	//---------------------Scenario 1 ^^^^^-----------
+	
+	//---------------------Scenario 2-----------
+	@Given("I navigate to Manifest List Page")
+	public void i_navigate_to_Manifest_List_Page() {
+
+	}
+
+	@Given("I enter Arrived for Status")
+	public void i_enter_Arrived_for_Status() {
+	
+	}
+
+	@Given("I enter Baltimore for Arrival Port")
+	public void i_enter_Baltimore_for_Arrival_Port() {
+
+	}
+
+	@Given("I should see a list of manifest for those two requests")
+	public void i_should_see_a_list_of_manifest_for_those_two_requests() {
+	    
+	}
+	//---------------------Scenario 2^^^^-----------
+
+}
